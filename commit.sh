06b47bbdd357bec6d573594d44ca618a375e80bd
bensrob <benjamin.stuart.roberts@gmail.com>
2017-04-09 02:08:43 +0000

    Added ignore

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git add .gitignore
 git add * 2> /dev/null
 if [ $(git ls-files --deleted -z | grep -cP ".*") -gt 0 ]; then
 	git ls-files --deleted -z | xargs -0 git rm --ignore-unmatch
