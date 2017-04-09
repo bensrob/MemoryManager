261ce0761c10e4b6390a6c8b8ec8e72bf75b4b2f
bensrob <benjamin.stuart.roberts@gmail.com>
2017-04-09 01:41:44 +0000

    /shrug I'm lazy

---
+++ commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git add * 2> /dev/null
+if [ $(git ls-files --deleted -z | grep -cP ".*") -gt 0 ]; then
+	git ls-files --deleted -z | xargs -0 git rm --ignore-unmatch
+fi
+git commit
+git push
