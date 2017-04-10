92a0fc59e18297b56516dcca2762d8e5cb7897aa
bensrob <benjamin.stuart.roberts@gmail.com>
2017-04-10 08:13:20 +0000

    Changed precompiled header guard to PCH

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .gitignore
-git add * 2> /dev/null
-if [ $(git ls-files --deleted -z | grep -cP ".*") -gt 0 ]; then
-	git ls-files --deleted -z | xargs -0 git rm --ignore-unmatch
-fi
-git commit
-git push
